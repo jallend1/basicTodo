51ac57bcbf2aa1808b04233267e492d2188419b9
jallend1 <jallend1@gmail.com>
2021-03-17 16:25:39 -0700

    Completes list display functionality and ability to add items to list

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,6 +1,23 @@
+import { useState } from 'react';
+
 const List = () => {
+    const [todos, setTodos] = useState([]);
+    const addTodo = (e) => {
+        e.preventDefault();   
+        const newTodos = todos.slice();
+        newTodos.push(e.target.item.value);
+        setTodos(newTodos);
+        e.target.reset();
+    }
+
     return(
-        <h1>List stuff</h1>
+        <>
+            <h1>The List</h1>
+            {todos.length ? todos.map(todo => <p>{todo}</p>) : <p>Nothing here yet!</p>}
+            <form onSubmit={addTodo}>
+                <input type="text" id="item" placeholder="Enter a new item"/>
+            </form>
+        </>
     )
 }
 
